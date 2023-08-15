91f0065a1a00564d2a6d47c465dd5e1f7b2fec39
Anna Shatilova <anxet@mail.ru>
2023-08-16 01:04:32 +0300

    Созданы дополнительные компоненты

---
+++ src/components/PlayerButtonPrev.js
@@ -0,0 +1,14 @@
+function PlayerButtonPrev () {
+    return (
+        <div className="player__btn-prev">
+        <svg
+          className="player__btn-prev-svg"
+          alt="prev"
+        >
+          <use xlinkHref="img/icon/sprite.svg#icon-prev" />
+        </svg>
+      </div>
+    )
+}
+
+export default PlayerButtonPrev
--- src/components/TrackList.js
+++ src/components/TrackList.js
@@ -1,28 +1,13 @@
-import Track from "./Track"
+import Track from './Track'
+import Filter from './Filter'
+import Search from './Search'
 
 function TrackList() {
   return (
     <div className="main__centerblock centerblock">
-      <div className="centerblock__search search">
-        <svg className="search__svg">
-          <use xlinkHref="./public/img/icon/sprite.svg#icon-search" />
-        </svg>
-        <input
-          className="search__text"
-          type="search"
-          placeholder="Поиск"
-          name="search"
-        />
-      </div>
+      <Search />
       <h2 className="centerblock__h2">Треки</h2>
-      <div className="centerblock__filter filter">
-        <div className="filter__title">Искать по:</div>
-        <div className="filter__button button-author _btn-text">
-          исполнителю
-        </div>
-        <div className="filter__button button-year _btn-text">году выпуска</div>
-        <div className="filter__button button-genre _btn-text">жанру</div>
-      </div>
+      <Filter />
       <div className="centerblock__content">
         <div className="content__title playlist-title">
           <div className="playlist-title__col col01">Трек</div>
