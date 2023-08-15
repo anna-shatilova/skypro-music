cd755e1852991e3913b0695f3a85b41f9188f183
Anna Shatilova <anxet@mail.ru>
2023-08-16 00:09:29 +0300

    Декомпозиция верстки на 7 основных компонентов завершена

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css'
 import AudioPlayer from './components/AudioPlayer'
 import NavMenu from './components/NavMenu'
+import Sidebar from './components/Sidebar'
 import TrackList from './components/TrackList'
 
 function App() {
@@ -10,60 +11,9 @@ function App() {
         <main className="main">
           <NavMenu />
           <TrackList />
-          <div className="main__sidebar sidebar">
-            <div className="sidebar__personal">
-              <p className="sidebar__personal-name">Sergey.Ivanov</p>
-              <div className="sidebar__icon">
-                <svg alt="logout">
-                  <use xlinkHref="img/icon/sprite.svg#logout" />
-                </svg>
-              </div>
-            </div>
-            <div className="sidebar__block">
-              <div className="sidebar__list">
-                <div className="sidebar__item">
-                  <a
-                    className="sidebar__link"
-                    href="http://"
-                  >
-                    <img
-                      className="sidebar__img"
-                      src="img/playlist01.png"
-                      alt="day's playlist"
-                    />
-                  </a>
-                </div>
-                <div className="sidebar__item">
-                  <a
-                    className="sidebar__link"
-                    href="http://"
-                  >
-                    <img
-                      className="sidebar__img"
-                      src="img/playlist02.png"
-                      alt="day's playlist"
-                    />
-                  </a>
-                </div>
-                <div className="sidebar__item">
-                  <a
-                    className="sidebar__link"
-                    href="http://"
-                  >
-                    <img
-                      className="sidebar__img"
-                      src="img/playlist03.png"
-                      alt="day's playlist"
-                    />
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
+          <Sidebar />
         </main>
-        <div className="bar">
-          <AudioPlayer />
-        </div>
+        <AudioPlayer />
         <footer className="footer" />
       </div>
     </div>
