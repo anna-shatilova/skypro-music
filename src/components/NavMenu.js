d234211ec1642d694ff6f8e73685bf18e7ac107f
Anna Shatilova <anxet@mail.ru>
2023-08-21 21:11:22 +0300

    Реализована работа бургер-меню по клику

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -1,4 +1,12 @@
+import { useState } from 'react'
+import NavMenuList from './NavMenuList'
+
 function NavMenu() {
+  const [openBurger, setOpenBurger] = useState(false)
+  function toggleOpenBurger() {
+    setOpenBurger(!openBurger)
+  }
+
   return (
     <nav className="main__nav nav">
       <div className="nav__logo logo">
@@ -8,39 +16,16 @@ function NavMenu() {
           alt="logo"
         />
       </div>
-      <div className="nav__burger burger">
+      <div
+        onClick={toggleOpenBurger}
+        aria-hidden="true"
+        className="nav__burger burger"
+      >
         <span className="burger__line" />
         <span className="burger__line" />
         <span className="burger__line" />
       </div>
-      <div className="nav__menu menu">
-        <ul className="menu__list">
-          <li className="menu__item">
-            <a
-              href="!#"
-              className="menu__link"
-            >
-              Главное
-            </a>
-          </li>
-          <li className="menu__item">
-            <a
-              href="!#"
-              className="menu__link"
-            >
-              Мой плейлист
-            </a>
-          </li>
-          <li className="menu__item">
-            <a
-              href="../signin.html"
-              className="menu__link"
-            >
-              Войти
-            </a>
-          </li>
-        </ul>
-      </div>
+      {openBurger ? <NavMenuList /> : ''}
     </nav>
   )
 }
