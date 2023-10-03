5f105b6c897aa002d7eeb0ca1dc52b4c2b36c8bb
Anna Shatilova <anxet@mail.ru>
2023-10-03 19:21:33 +0300

    Реализованы play/pause в аудиоплеере.  Использован useEffect, чтобы запускать воспроизведение при смене трека

--- src/components/AudioPlayer/AudioPlayer.js
+++ src/components/AudioPlayer/AudioPlayer.js
@@ -1,13 +1,38 @@
+import { useEffect, useRef, useState } from 'react'
 import * as S from './Styles'
 import { Buttons } from './Buttons/Buttons'
 import { Track } from './Track/Track'
 
 export const AudioPlayer = ({ currentTrack }) => {
+  const [isPlaying, setIsPlaying] = useState(false)
+  const audioRef = useRef(null)
+
+  // запуск/пауза
+  const handleStart = () => {
+    audioRef.current.play()
+    setIsPlaying(true)
+  }
+
+  const handleStop = () => {
+    audioRef.current.pause()
+    setIsPlaying(false)
+  }
+
+  const togglePlay = isPlaying ? handleStop : handleStart
+
+  useEffect(() => {
+    if (audioRef.current) {
+      audioRef.current.currentTime = 0
+      handleStart()
+    }
+  }, [currentTrack])
+
   return (
     <S.Bar>
       <S.BarContent>
         <audio
           controls
+          ref={audioRef}
           src={currentTrack.track_file}
           autoPlay
         >
@@ -17,7 +42,10 @@ export const AudioPlayer = ({ currentTrack }) => {
         <S.BarPlayerProgress />
         <S.BarPlayerBlock>
           <S.BarPlayer>
-            <Buttons />
+            <Buttons
+              togglePlay={togglePlay}
+              isPlaying={isPlaying}
+            />
             <Track currentTrack={currentTrack} />
           </S.BarPlayer>
           <S.BarVolumeBlock>
--- src/components/AudioPlayer/Buttons/Buttons.js
+++ src/components/AudioPlayer/Buttons/Buttons.js
@@ -1,19 +1,32 @@
 import * as S from './Styles'
 
-export const Buttons = () => {
+export const Buttons = ({ isPlaying, togglePlay }) => {
+  const handleNotRealized = () => {
+    alert('Эта функция еще не реализована')
+  }
+
   return (
     <S.PlayerControls>
-      <S.PlayerBtnPrev>
+      <S.PlayerBtnPrev onClick={handleNotRealized}>
         <S.PlayerBtnPrevSvg alt="prev">
           <use xlinkHref="img/icon/sprite.svg#icon-prev" />
         </S.PlayerBtnPrevSvg>
       </S.PlayerBtnPrev>
-      <S.PlayerBtnPlay className="_btn">
-        <S.PlayerBtnPlaySvg alt="play">
-          <use xlinkHref="img/icon/sprite.svg#icon-play" />
-        </S.PlayerBtnPlaySvg>
+      <S.PlayerBtnPlay
+        className="_btn"
+        onClick={togglePlay}
+      >
+        {isPlaying ? (
+          <S.PlayerBtnPlaySvg alt="pause">
+            <use xlinkHref="img/icon/sprite.svg#icon-pause" />
+          </S.PlayerBtnPlaySvg>
+        ) : (
+          <S.PlayerBtnPlaySvg alt="play">
+            <use xlinkHref="img/icon/sprite.svg#icon-play" />
+          </S.PlayerBtnPlaySvg>
+        )}
       </S.PlayerBtnPlay>
-      <S.PlayerBtnNext>
+      <S.PlayerBtnNext onClick={handleNotRealized}>
         <S.PlayerBtnNextSvg alt="next">
           <use xlinkHref="img/icon/sprite.svg#icon-next" />
         </S.PlayerBtnNextSvg>
@@ -23,7 +36,10 @@ export const Buttons = () => {
           <use xlinkHref="img/icon/sprite.svg#icon-repeat" />
         </S.PlayerBtnRepeatSvg>
       </S.PlayerBtnRepeat>
-      <S.PlayerBtnShuffle className="_btn-icon">
+      <S.PlayerBtnShuffle
+        className="_btn-icon"
+        onClick={handleNotRealized}
+      >
         <S.PlayerBtnShuffleSvg alt="shuffle">
           <use xlinkHref="img/icon/sprite.svg#icon-shuffle" />
         </S.PlayerBtnShuffleSvg>
