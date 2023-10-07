3ac07e9f1b328e8a65014d72bdcff9b4afe4ac27
Anna Shatilova <anxet@mail.ru>
2023-10-07 23:42:36 +0300

    Созданы страницы входа и регистрации. Добавлена валидация полей.

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,26 +1,106 @@
-import { Link, useNavigate } from 'react-router-dom'
+// import { Link, useNavigate } from 'react-router-dom'
+
+// export const Login = () => {
+//   const navigate = useNavigate()
+
+//   const setUser = () => {
+//     localStorage.setItem('user', 'token')
+//     navigate('/', { replace: true })
+//   }
+//   return (
+//     <>
+//       <h1>Страница логина</h1>
+//       <div className="modal__btn-enter">
+//         <Link
+//           to="/"
+//           onClick={setUser}
+//         >
+//           Войти
+//         </Link>
+//       </div>
+//       <div className="modal__btn-signup">
+//         <Link to="/register">Зарегистрироваться</Link>
+//       </div>
+//     </>
+//   )
+// }
+
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import * as S from './LoginAndRegister.styles'
 
 export const Login = () => {
-  const navigate = useNavigate()
+  const [loginError, setLoginError] = useState(null)
 
-  const setUser = () => {
-    localStorage.setItem('user', 'token')
-    navigate('/', { replace: true })
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleLogin = async () => {
+    try {
+      if (!email) {
+        setLoginError('Введите email')
+        return
+      }
+
+      if (!password) {
+        setLoginError('Введите пароль')
+      }
+      // await localStorage.setItem('user', 'token')
+
+      // await loginUser({ email, password }).then((loginData) => {
+      //   getToken({ email, password }).then((tokenData) => {
+      //     login(loginData, tokenData.access);
+      //   });
+      // });
+    } catch (error) {
+      setLoginError(error.message)
+    }
   }
+
+  // Сбрасываем ошибку если пользователь меняет данные на форме или меняется режим формы
+  useEffect(() => {
+    setLoginError(null)
+  }, [email, password])
+
   return (
-    <>
-      <h1>Страница логина</h1>
-      <div className="modal__btn-enter">
-        <Link
-          to="/"
-          onClick={setUser}
-        >
-          Войти
+    <S.PageContainer>
+      <S.ModalForm>
+        <Link to="/login">
+          <S.ModalLogo>
+            <S.ModalLogoImage
+              src="/img/logo_modal.png"
+              alt="logo"
+            />
+          </S.ModalLogo>
         </Link>
-      </div>
-      <div className="modal__btn-signup">
-        <Link to="/register">Зарегистрироваться</Link>
-      </div>
-    </>
+        <S.Inputs>
+          <S.ModalInput
+            type="text"
+            name="login"
+            placeholder="Почта"
+            value={email}
+            onChange={(event) => {
+              setEmail(event.target.value)
+            }}
+          />
+          <S.ModalInput
+            type="password"
+            name="password"
+            placeholder="Пароль"
+            value={password}
+            onChange={(event) => {
+              setPassword(event.target.value)
+            }}
+          />
+        </S.Inputs>
+        {loginError && <S.Error>{loginError}</S.Error>}
+        <S.Buttons>
+          <S.PrimaryButton onClick={handleLogin}>Войти</S.PrimaryButton>
+          <Link to="/register">
+            <S.SecondaryButton>Зарегистрироваться</S.SecondaryButton>
+          </Link>
+        </S.Buttons>
+      </S.ModalForm>
+    </S.PageContainer>
   )
 }
--- src/pages/register/index.jsx
+++ src/pages/register/index.jsx
@@ -1,6 +1,96 @@
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import * as S from '../login/LoginAndRegister.styles'
+
 export const Register = () => {
-    return (
-        <h1>Register Page</h1>
-    )
+  const [regError, setRegError] = useState(null)
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [repeatPassword, setRepeatPassword] = useState('')
+
+  const handleRegister = async () => {
+    try {
+      if (!email) {
+        setRegError('Введите email')
+        return
+      }
+
+      if (!password) {
+        setRegError('Введите пароль')
+        return
+      }
+
+      if (!repeatPassword) {
+        setRegError('Подтвердите пароль')
+        return
+      }
+
+      if (password !== repeatPassword) {
+        setRegError('Пароли не совпадают')
+      }
+      // await registerUser({ email, password }).then((loginData) => {
+      //     getToken({ email, password }).then((tokenData) => {
+      //       login(loginData, tokenData.access);
+      //     });
+      //   });
+    } catch (error) {
+      setRegError(error.message)
+    }
   }
-  
+
+  // Сбрасываем ошибку если пользователь меняет данные на форме или меняется режим формы
+  useEffect(() => {
+    setRegError(null)
+  }, [email, password, repeatPassword])
+
+  return (
+    <S.PageContainer>
+      <S.ModalForm>
+        <Link to="/login">
+          <S.ModalLogo>
+            <S.ModalLogoImage
+              src="/img/logo_modal.png"
+              alt="logo"
+            />
+          </S.ModalLogo>
+        </Link>
+        <S.Inputs>
+          <S.ModalInput
+            type="text"
+            name="login"
+            placeholder="Почта"
+            value={email}
+            onChange={(event) => {
+              setEmail(event.target.value)
+            }}
+          />
+          <S.ModalInput
+            type="password"
+            name="password"
+            placeholder="Пароль"
+            value={password}
+            onChange={(event) => {
+              setPassword(event.target.value)
+            }}
+          />
+          <S.ModalInput
+            type="password"
+            name="repeat-password"
+            placeholder="Повторите пароль"
+            value={repeatPassword}
+            onChange={(event) => {
+              setRepeatPassword(event.target.value)
+            }}
+          />
+        </S.Inputs>
+        {regError && <S.Error>{regError}</S.Error>}
+        <S.Buttons>
+          <S.PrimaryButton onClick={handleRegister}>
+            Зарегистрироваться
+          </S.PrimaryButton>
+        </S.Buttons>
+      </S.ModalForm>
+    </S.PageContainer>
+  )
+}
